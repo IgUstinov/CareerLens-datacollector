b2c7717adb224772c3dfea7dab478d33f5ff19a9
fey_ko2 <fey_ko2@git.imbalanced.tech>
2024-11-17 14:47:45 +0300

    fix request

--- app/Kafka/KafkaProvider.py
+++ app/Kafka/KafkaProvider.py
@@ -39,12 +39,12 @@ class KafkaProvider:
     async def start_collect_data(self, msg):
         logging.info("Start collect data from hh ru")
         collectData = msg.get('collectData')
-        requestsId = msg.get('requestsId')
+        requestId = msg.get('requestId')
         async for batch in self.collector.get_vacancies_batches(collectData['count']):
             result = await self.dbProvider.get_NoFilerRepo().post_data(batch)
             logging.info("Add data: ", result)
             logging.info(result.items())
-            added = {"Ids": result, "requestsId":requestsId}
+            added = {"Ids": result, "requestId":requestId}
             await self.added_data_to_mongodb(added)
 
     async def added_data_to_mongodb(self, ids):
@@ -55,7 +55,7 @@ class KafkaProvider:
         try:
             # Преобразуем сообщение из Kafka в JSON
             filter_data = message.get("filterData", {})
-            request_id = message.get("requestId")  # Предполагаем, что requestId приходит с запросом
+            requestId = message.get("requestId")  # Предполагаем, что requestId приходит с запросом
 
             mongo_filter = await self.flatten_dict(filter_data)
             logging.info(mongo_filter)
@@ -71,7 +71,7 @@ class KafkaProvider:
                                                                                          filtered_data)
                 # Сохраняем метаданные коллекции
                 metadata = {
-                    "request_id": request_id,
+                    "requestId": requestId,
                     "timestamp": timestamp,
                     "filter_data": filter_data,
                     "collection_name": new_collection_name
@@ -79,7 +79,7 @@ class KafkaProvider:
                 await self.dbProvider.get_NoFilerRepo().create_metadata(metadata)
 
                 # Отправляем имя коллекции в Kafka как ответ
-                response = {"collectionName": new_collection_name, "request_id": request_id}
+                response = {"collectionName": new_collection_name, "requestId": requestId}
                 logging.info(response)
                 producer = ProducerKafka()
                 await producer.deliver_message(json.dumps(response), "Python")
@@ -91,6 +91,12 @@ class KafkaProvider:
     async def flatten_dict(self, data, parent_key='', separator='.'):
         items = {}
         for k, v in data.items():
+            if(k=='salary'):
+                items['$or'] = [
+                        {'salary.from': {'$gte': v['value']}},  # from больше или равно указанного значения
+                        {'salary.to': {'$lt': v['value']}}  # to меньше указанного значения
+                    ]
+                continue
             new_key = f"{parent_key}{separator}{k}" if parent_key else k
             if isinstance(v, dict):
                 items.update(await self.flatten_dict(v, new_key, separator=separator))
@@ -99,8 +105,8 @@ class KafkaProvider:
         return items
 
     async def CountryRequest(self, msg):
-        requestsId = msg.get('requestsId')
+        requestId = msg.get('requestId')
         Countries = await self.collector.get_countries()
         result = await self.dbProvider.get_NoFilerRepo().add_countries(Countries)
         logging.info("Add data: ", result)
-        await self.added_data_to_mongodb({"Ids": result, "requestsId": requestsId})
+        await self.added_data_to_mongodb({"Ids": result, "requestId": requestId})
